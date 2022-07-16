4c0bd592846f8377bc0ff89d7569d0fe9f5474f8
ikuvamzz <76558264+ikuvamzz@users.noreply.github.com>
2022-07-16 14:17:56 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -19,9 +19,9 @@ const seo = {
 
 //Home Page
 const greeting = {
-  title: "Ashutosh Hathidara",
-  logo_name: "AshutoshHathidara",
-  nickname: "layman_brother",
+  title: "Muhammedh Ikram",
+  logo_name: "MuhammedhIkram",
+  nickname: "ikuvam",
   subTitle:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   resumeLink:
